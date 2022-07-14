5539f09b49a3f53e559be200cdffa83044d2bc60
hbin889 <hbin889@gmail.com>
2022-07-14 23:07:00 +0900

    Fin

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -29,7 +29,7 @@ class HomePage extends StatelessWidget {
               ),
             ),
             Container(
-              margin: EdgeInsets.only(left: 280),
+              margin: EdgeInsets.only(left: 260),
               child: ElevatedButton(
                 onPressed: () {},
                 style: ButtonStyle(
