78b7e86bfeecd96384c21e792418543c0d200784
hbin889 <hbin889@gmail.com>
2022-07-23 16:02:50 +0900

    shelf_expanded

--- lib/pages/my_page.dart
+++ lib/pages/my_page.dart
@@ -13,167 +13,109 @@ class MyPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
+    return Padding(
+      padding: EdgeInsets.only(top: 30, left: 25, right: 25),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            alignment: Alignment.topLeft,
-            child: Padding(
-              padding:
-                  EdgeInsets.only(top: 30, left: 25, right: 25, bottom: 20),
-              child: Text(
-                '내가 받은 화분 12',
-                style: Theme.of(context).textTheme.headline2,
+          Text(
+            "내가 받은 화분 12",
+            style: Theme.of(context).textTheme.headline2,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: Container(
+                width: double.infinity, // 폭
+                margin: EdgeInsets.only(top: 10),
+                decoration: BoxDecoration(
+                    // 박스 색상
+                    ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                        child: Column(children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Image.asset('assets/icons/vase1.png',
+                                fit: BoxFit.cover),
+                          ),
+                          Expanded(
+                            child: Image.asset('assets/icons/vase2.png',
+                                fit: BoxFit.cover),
+                          ),
+                          Expanded(
+                            child: Image.asset('assets/icons/vase1.png',
+                                fit: BoxFit.cover),
+                          ),
+                        ],
+                      ),
+                      Image.asset(
+                        'assets/icons/shelf1.png',
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      )
+                    ])),
+                    Expanded(
+                        child: Column(children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Image.asset('assets/icons/vase2.png',
+                                fit: BoxFit.cover),
+                          ),
+                          Expanded(
+                            child: Image.asset('assets/icons/vase1.png',
+                                fit: BoxFit.cover),
+                          ),
+                          Expanded(
+                            child: Image.asset('assets/icons/vase2.png',
+                                fit: BoxFit.cover),
+                          ),
+                        ],
+                      ),
+                      Image.asset(
+                        'assets/icons/shelf2.png',
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      )
+                    ])),
+                    Expanded(
+                        child: Column(children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Image.asset('assets/icons/vase1.png',
+                                fit: BoxFit.cover),
+                          ),
+                          Expanded(
+                            child: Image.asset('assets/icons/vase2.png',
+                                fit: BoxFit.cover),
+                          ),
+                          Expanded(
+                            child: Image.asset('assets/icons/vase2.png',
+                                fit: BoxFit.cover),
+                          ),
+                        ],
+                      ),
+                      Image.asset(
+                        'assets/icons/shelf3.png',
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      )
+                    ])),
+                  ],
+                ),
               ),
             ),
-          ),
-          Column(
-            children: [
-              Container(
-                width: double.infinity,
-                height: 140,
-                margin: EdgeInsets.only(top: 0),
-                padding: EdgeInsets.only(top: 10, left: 10, right: 10),
-                child: GridView.count(
-                  crossAxisCount: 3,
-                  children: [
-                    Image.asset('assets/icons/tree.png'),
-                    Image.asset('assets/icons/tree.png'),
-                    Image.asset('assets/icons/tree.png'),
-                  ],
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(left: 25, right: 25),
-                width: double.infinity,
-                child: Image.asset(
-                  'assets/icons/Rectangle 40.png',
-                  fit: BoxFit.cover,
-                ),
-              )
-            ],
-          ),
-          Column(
-            children: [
-              Container(
-                width: double.infinity,
-                height: 140,
-                margin: EdgeInsets.only(top: 0),
-                padding: EdgeInsets.only(top: 10, left: 10, right: 10),
-                child: GridView.count(
-                  crossAxisCount: 3,
-                  children: [
-                    Image.asset('assets/icons/tree.png'),
-                    Image.asset('assets/icons/tree.png'),
-                    Image.asset('assets/icons/tree.png'),
-                  ],
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(left: 25, right: 25),
-                width: double.infinity,
-                child: Image.asset(
-                  'assets/icons/Rectangle 40.png',
-                  fit: BoxFit.cover,
-                ),
-              )
-            ],
-          ),
-          Column(
-            children: [
-              Container(
-                width: double.infinity,
-                height: 140,
-                margin: EdgeInsets.only(top: 0),
-                padding: EdgeInsets.only(top: 10, left: 10, right: 10),
-                child: GridView.count(
-                  crossAxisCount: 3,
-                  children: [
-                    Image.asset('assets/icons/tree.png'),
-                    Image.asset('assets/icons/tree.png'),
-                    Image.asset('assets/icons/tree.png'),
-                  ],
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.only(left: 25, right: 25),
-                width: double.infinity,
-                child: Image.asset(
-                  'assets/icons/Rectangle 40.png',
-                  fit: BoxFit.cover,
-                ),
-              )
-            ],
           ),
         ],
       ),
     );
-    // return Padding(
-    //   padding: EdgeInsets.only(top: 30, left: 25, right: 25),
-    //   child: Column(
-    //     crossAxisAlignment: CrossAxisAlignment.start,
-    //     children: [
-    //       Text(
-    //         "내가 받은 화분 12",
-    //         style: Theme.of(context).textTheme.headline2,
-    //       ),
-    //       Center(
-    //         child: Container(
-    //           width: double.infinity, // 폭
-    //           margin: EdgeInsets.only(top: 10),
-    //           height: 470, // 높이
-    //           decoration: BoxDecoration(
-    //             color: Color.fromARGB(255, 255, 255, 255), // 박스 색상
-    //           ),
-
-    //           child: Column(
-    //             children: [
-    //               Column(
-    //                 children: [
-    //                   Row(
-    //                     mainAxisAlignment: MainAxisAlignment.center,
-    //                     children: [
-    //                       Container(
-    //                         padding: EdgeInsets.fromLTRB(10, 8, 10, 0),
-    //                         child: Image.asset('assets/icons/tree.png'),
-    //                       ),
-    //                       Container(
-    //                         padding: EdgeInsets.fromLTRB(10, 8, 10, 0),
-    //                         child: Image.asset('assets/icons/tree.png'),
-    //                       ),
-    //                       Container(
-    //                         padding: EdgeInsets.fromLTRB(10, 8, 10, 0),
-    //                         child: Image.asset('assets/icons/tree.png'),
-    //                       ),
-    //                     ],
-    //                   ),
-    //                   Container(
-    //                     child: Image.asset(
-    //                       'assets/icons/Rectangle 40.png',
-    //                       width: double.infinity,
-    //                       fit: BoxFit.cover,
-    //                     ),
-    //                   ),
-    //                 ],
-    //               ),
-    //             ],
-    //           ),
-    //         ),
-    //       ),
-    //       TextButton(
-    //         onPressed: () {
-    //           FirebaseAuth.instance.signOut();
-    //           Navigator.push(
-    //             context,
-    //             MaterialPageRoute(builder: (context) => StartPage()),
-    //           );
-    //         },
-    //         child: Text(
-    //           '로그아웃(디버깅용)',
-    //         ),
-    //       ),
-    //     ],
-    //   ),
-    // );
   }
 }
