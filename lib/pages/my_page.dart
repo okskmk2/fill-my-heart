2dc034b87f0569c0d55309a64c528580e25d1e67
hbin889 <hbin889@gmail.com>
2022-07-23 13:05:27 +0900

    my page

--- lib/pages/my_page.dart
+++ lib/pages/my_page.dart
@@ -13,116 +13,167 @@ class MyPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    /*return Container(
-      alignment: AlignmentDirectional.center,
-      child: Image.asset('assets/tree.jpg'),
-    );*/
-    return Padding(
-      padding: EdgeInsets.only(top: 30, left: 20, right: 10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+    return Scaffold(
+      body: Column(
         children: [
-          Text(
-            "내가 받은 화분 12",
-            style: Theme.of(context).textTheme.headline2,
-          ),
           Container(
-            width: double.infinity, // 폭
-            margin: EdgeInsets.only(top: 10),
-            height: 470, // 높이
-            decoration: BoxDecoration(color: Colors.white // 박스 색상
-                ),
-            child: Column(
-              children: [
-                Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          padding: EdgeInsets.fromLTRB(3, 8, 3, 0),
-                          child: Image.asset('assets/icons/tree.png'),
-                        ),
-                        Container(
-                          padding: EdgeInsets.fromLTRB(3, 8, 3, 0),
-                          child: Image.asset('assets/icons/tree.png'),
-                        ),
-                        Container(
-                          padding: EdgeInsets.fromLTRB(3, 8, 3, 0),
-                          child: Image.asset('assets/icons/tree.png'),
-                        ),
-                      ],
-                    ),
-                    Container(
-                      child: Image.asset('assets/icons/Rectangle 40.png'),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          padding: EdgeInsets.fromLTRB(3, 8, 3, 0),
-                          child: Image.asset('assets/icons/tree.png'),
-                        ),
-                        Container(
-                          padding: EdgeInsets.fromLTRB(3, 8, 3, 0),
-                          child: Image.asset('assets/icons/tree.png'),
-                        ),
-                        Container(
-                          padding: EdgeInsets.fromLTRB(3, 8, 3, 0),
-                          child: Image.asset('assets/icons/tree.png'),
-                        ),
-                      ],
-                    ),
-                    Container(
-                      child: Image.asset('assets/icons/Rectangle 40.png'),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          padding: EdgeInsets.fromLTRB(3, 8, 3, 0),
-                          child: Image.asset('assets/icons/tree.png'),
-                        ),
-                        Container(
-                          padding: EdgeInsets.fromLTRB(3, 8, 3, 0),
-                          child: Image.asset('assets/icons/tree.png'),
-                        ),
-                        Container(
-                          padding: EdgeInsets.fromLTRB(3, 8, 3, 0),
-                          child: Image.asset('assets/icons/tree.png'),
-                        ),
-                      ],
-                    ),
-                    Container(
-                      child: Image.asset('assets/icons/Rectangle 40.png'),
-                    ),
-                  ],
-                ),
-              ],
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding:
+                  EdgeInsets.only(top: 30, left: 25, right: 25, bottom: 20),
+              child: Text(
+                '내가 받은 화분 12',
+                style: Theme.of(context).textTheme.headline2,
+              ),
             ),
           ),
-          TextButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => StartPage()),
-              );
-            },
-            child: Text(
-              '로그아웃(디버깅용)',
-            ),
+          Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 140,
+                margin: EdgeInsets.only(top: 0),
+                padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+                child: GridView.count(
+                  crossAxisCount: 3,
+                  children: [
+                    Image.asset('assets/icons/tree.png'),
+                    Image.asset('assets/icons/tree.png'),
+                    Image.asset('assets/icons/tree.png'),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 25, right: 25),
+                width: double.infinity,
+                child: Image.asset(
+                  'assets/icons/Rectangle 40.png',
+                  fit: BoxFit.cover,
+                ),
+              )
+            ],
+          ),
+          Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 140,
+                margin: EdgeInsets.only(top: 0),
+                padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+                child: GridView.count(
+                  crossAxisCount: 3,
+                  children: [
+                    Image.asset('assets/icons/tree.png'),
+                    Image.asset('assets/icons/tree.png'),
+                    Image.asset('assets/icons/tree.png'),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 25, right: 25),
+                width: double.infinity,
+                child: Image.asset(
+                  'assets/icons/Rectangle 40.png',
+                  fit: BoxFit.cover,
+                ),
+              )
+            ],
+          ),
+          Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 140,
+                margin: EdgeInsets.only(top: 0),
+                padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+                child: GridView.count(
+                  crossAxisCount: 3,
+                  children: [
+                    Image.asset('assets/icons/tree.png'),
+                    Image.asset('assets/icons/tree.png'),
+                    Image.asset('assets/icons/tree.png'),
+                  ],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 25, right: 25),
+                width: double.infinity,
+                child: Image.asset(
+                  'assets/icons/Rectangle 40.png',
+                  fit: BoxFit.cover,
+                ),
+              )
+            ],
           ),
         ],
       ),
     );
+    // return Padding(
+    //   padding: EdgeInsets.only(top: 30, left: 25, right: 25),
+    //   child: Column(
+    //     crossAxisAlignment: CrossAxisAlignment.start,
+    //     children: [
+    //       Text(
+    //         "내가 받은 화분 12",
+    //         style: Theme.of(context).textTheme.headline2,
+    //       ),
+    //       Center(
+    //         child: Container(
+    //           width: double.infinity, // 폭
+    //           margin: EdgeInsets.only(top: 10),
+    //           height: 470, // 높이
+    //           decoration: BoxDecoration(
+    //             color: Color.fromARGB(255, 255, 255, 255), // 박스 색상
+    //           ),
+
+    //           child: Column(
+    //             children: [
+    //               Column(
+    //                 children: [
+    //                   Row(
+    //                     mainAxisAlignment: MainAxisAlignment.center,
+    //                     children: [
+    //                       Container(
+    //                         padding: EdgeInsets.fromLTRB(10, 8, 10, 0),
+    //                         child: Image.asset('assets/icons/tree.png'),
+    //                       ),
+    //                       Container(
+    //                         padding: EdgeInsets.fromLTRB(10, 8, 10, 0),
+    //                         child: Image.asset('assets/icons/tree.png'),
+    //                       ),
+    //                       Container(
+    //                         padding: EdgeInsets.fromLTRB(10, 8, 10, 0),
+    //                         child: Image.asset('assets/icons/tree.png'),
+    //                       ),
+    //                     ],
+    //                   ),
+    //                   Container(
+    //                     child: Image.asset(
+    //                       'assets/icons/Rectangle 40.png',
+    //                       width: double.infinity,
+    //                       fit: BoxFit.cover,
+    //                     ),
+    //                   ),
+    //                 ],
+    //               ),
+    //             ],
+    //           ),
+    //         ),
+    //       ),
+    //       TextButton(
+    //         onPressed: () {
+    //           FirebaseAuth.instance.signOut();
+    //           Navigator.push(
+    //             context,
+    //             MaterialPageRoute(builder: (context) => StartPage()),
+    //           );
+    //         },
+    //         child: Text(
+    //           '로그아웃(디버깅용)',
+    //         ),
+    //       ),
+    //     ],
+    //   ),
+    // );
   }
 }
