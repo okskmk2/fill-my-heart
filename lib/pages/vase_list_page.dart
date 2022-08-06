0f20d0213a55ae982976dff6478732b511908581
hbin889 <hbin889@gmail.com>
2022-08-06 15:12:55 +0900

    Vase_list_background

--- lib/pages/vase_list_page.dart
+++ lib/pages/vase_list_page.dart
@@ -8,186 +8,193 @@ class VaseListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(top: 30, left: 25, right: 25),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "내가 받은 화분 12",
-            style: Theme.of(context).textTheme.headline2,
-          ),
-          SizedBox(height: 30),
-          Expanded(
-            flex: 1,
-            child: Swiper(
-              control: SwiperControl(),
-              pagination: SwiperPagination(),
-              loop: false,
-              itemCount: 1,
-              itemBuilder: (BuildContext context, int index) {
-                return Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Expanded(
-                        child: Column(children: [
-                      Row(
-                        children: [
-                          Expanded(
-                            child: GestureDetector(
-                              onTap: () {
-                                Navigator.pushNamed(context, '/vase-detail');
-                              },
-                              child: Image.asset(
-                                'assets/icons/vase1.png',
-                                height: vaseSize,
-                                fit: BoxFit.scaleDown,
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            child: GestureDetector(
-                              onTap: () {
-                                Navigator.pushNamed(context, '/vase-detail');
-                              },
-                              child: Image.asset(
-                                'assets/icons/vase2.png',
-                                height: vaseSize,
-                                fit: BoxFit.scaleDown,
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            child: GestureDetector(
-                              onTap: () {
-                                Navigator.pushNamed(context, '/vase-detail');
-                              },
-                              child: Image.asset(
-                                'assets/icons/vase1.png',
-                                height: vaseSize,
-                                fit: BoxFit.scaleDown,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Image.asset(
-                        'assets/icons/shelf1.png',
-                        width: double.infinity,
-                        fit: BoxFit.cover,
-                      )
-                    ])),
-                    Expanded(
-                        child: Column(children: [
-                      Row(
-                        children: [
-                          Expanded(
-                            child: GestureDetector(
-                              onTap: () {
-                                Navigator.pushNamed(context, '/vase-detail');
-                              },
-                              child: Image.asset(
-                                'assets/icons/vase2.png',
-                                height: vaseSize,
-                                fit: BoxFit.scaleDown,
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            child: GestureDetector(
-                              onTap: () {
-                                Navigator.pushNamed(context, '/vase-detail');
-                              },
-                              child: Image.asset(
-                                'assets/icons/vase1.png',
-                                height: vaseSize,
-                                fit: BoxFit.scaleDown,
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            child: GestureDetector(
-                              onTap: () {
-                                Navigator.pushNamed(context, '/vase-detail');
-                              },
-                              child: Image.asset(
-                                'assets/icons/vase2.png',
-                                height: vaseSize,
-                                fit: BoxFit.scaleDown,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      Image.asset(
-                        'assets/icons/shelf2.png',
-                        width: double.infinity,
-                        fit: BoxFit.cover,
-                      )
-                    ])),
-                    Expanded(
-                      child: Column(
-                        children: [
-                          Row(
-                            children: [
-                              Expanded(
-                                child: GestureDetector(
-                                  onTap: () {
-                                    Navigator.pushNamed(
-                                        context, '/vase-detail');
-                                  },
-                                  child: Image.asset(
-                                    'assets/icons/vase1.png',
-                                    height: vaseSize,
-                                    fit: BoxFit.scaleDown,
-                                  ),
-                                ),
-                              ),
-                              Expanded(
-                                child: GestureDetector(
-                                  onTap: () {
-                                    Navigator.pushNamed(
-                                        context, '/vase-detail');
-                                  },
-                                  child: Image.asset(
-                                    'assets/icons/vase2.png',
-                                    height: vaseSize,
-                                    fit: BoxFit.scaleDown,
-                                  ),
-                                ),
-                              ),
-                              Expanded(
-                                child: GestureDetector(
-                                  onTap: () {
-                                    Navigator.pushNamed(
-                                        context, '/vase-detail');
-                                  },
-                                  child: Image.asset(
-                                    'assets/icons/vase2.png',
-                                    height: vaseSize,
-                                    fit: BoxFit.scaleDown,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          Image.asset(
-                            'assets/icons/shelf3.png',
-                            width: double.infinity,
-                            fit: BoxFit.cover,
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
-                );
-              },
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/VLbackground.png"),
+        ),
+      ),
+      child: Padding(
+        padding: EdgeInsets.only(top: 30, left: 25, right: 25),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "내가 받은 화분 12",
+              style: Theme.of(context).textTheme.headline2,
             ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-        ],
+            SizedBox(height: 30),
+            Expanded(
+              flex: 1,
+              child: Swiper(
+                control: SwiperControl(),
+                pagination: SwiperPagination(),
+                loop: false,
+                itemCount: 1,
+                itemBuilder: (BuildContext context, int index) {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(
+                          child: Column(children: [
+                        Row(
+                          children: [
+                            Expanded(
+                              child: GestureDetector(
+                                onTap: () {
+                                  Navigator.pushNamed(context, '/vase-detail');
+                                },
+                                child: Image.asset(
+                                  'assets/icons/vase1.png',
+                                  height: vaseSize,
+                                  fit: BoxFit.scaleDown,
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: GestureDetector(
+                                onTap: () {
+                                  Navigator.pushNamed(context, '/vase-detail');
+                                },
+                                child: Image.asset(
+                                  'assets/icons/vase2.png',
+                                  height: vaseSize,
+                                  fit: BoxFit.scaleDown,
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: GestureDetector(
+                                onTap: () {
+                                  Navigator.pushNamed(context, '/vase-detail');
+                                },
+                                child: Image.asset(
+                                  'assets/icons/vase1.png',
+                                  height: vaseSize,
+                                  fit: BoxFit.scaleDown,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Image.asset(
+                          'assets/icons/shelf1.png',
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        )
+                      ])),
+                      Expanded(
+                          child: Column(children: [
+                        Row(
+                          children: [
+                            Expanded(
+                              child: GestureDetector(
+                                onTap: () {
+                                  Navigator.pushNamed(context, '/vase-detail');
+                                },
+                                child: Image.asset(
+                                  'assets/icons/vase2.png',
+                                  height: vaseSize,
+                                  fit: BoxFit.scaleDown,
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: GestureDetector(
+                                onTap: () {
+                                  Navigator.pushNamed(context, '/vase-detail');
+                                },
+                                child: Image.asset(
+                                  'assets/icons/vase1.png',
+                                  height: vaseSize,
+                                  fit: BoxFit.scaleDown,
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: GestureDetector(
+                                onTap: () {
+                                  Navigator.pushNamed(context, '/vase-detail');
+                                },
+                                child: Image.asset(
+                                  'assets/icons/vase2.png',
+                                  height: vaseSize,
+                                  fit: BoxFit.scaleDown,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Image.asset(
+                          'assets/icons/shelf2.png',
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        )
+                      ])),
+                      Expanded(
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      Navigator.pushNamed(
+                                          context, '/vase-detail');
+                                    },
+                                    child: Image.asset(
+                                      'assets/icons/vase1.png',
+                                      height: vaseSize,
+                                      fit: BoxFit.scaleDown,
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      Navigator.pushNamed(
+                                          context, '/vase-detail');
+                                    },
+                                    child: Image.asset(
+                                      'assets/icons/vase2.png',
+                                      height: vaseSize,
+                                      fit: BoxFit.scaleDown,
+                                    ),
+                                  ),
+                                ),
+                                Expanded(
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      Navigator.pushNamed(
+                                          context, '/vase-detail');
+                                    },
+                                    child: Image.asset(
+                                      'assets/icons/vase2.png',
+                                      height: vaseSize,
+                                      fit: BoxFit.scaleDown,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Image.asset(
+                              'assets/icons/shelf3.png',
+                              width: double.infinity,
+                              fit: BoxFit.cover,
+                            )
+                          ],
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+          ],
+        ),
       ),
     );
   }
