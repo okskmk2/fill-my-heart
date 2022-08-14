f0d43393f8f9f891d77254e6863f6689b1711863
eunsung lee <okskmk2@naver.com>
2022-08-14 20:58:48 +0900

    docs test

--- web-vue/vue.config.js
+++ web-vue/vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  publicPath: process.env.NODE_ENV === "production" ? "/thank-tree/" : "/",
+  outputDir: "docs"
 })
